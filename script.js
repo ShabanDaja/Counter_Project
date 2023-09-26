5cf28edb92b7c2028198908bdbd9df1c6b885ec2
Shaban Daja <shabandaja710@gmail.com>
2023-09-26 15:04:00 +0200

    When i press ( - ) the counter decrese

--- script.js
+++ script.js
@@ -1,5 +1,17 @@
 const increaseButtonEl = document.querySelector('.counter__button--increase');
+const decreaseButtonEl = document.querySelector('.counter__button--deacrease');
 const counterValueEl = document.querySelector('.counter__value');
+
+decreaseButtonEl.addEventListener('click', function() {
+
+    const currentValue = counterValueEl.textContent;
+
+    const currentValueAsNumber = +currentValue;
+
+    const newValue = currentValueAsNumber -1;
+
+    counterValueEl.textContent = newValue;
+})
 
 increaseButtonEl.addEventListener('click', function() {
     // get current value
@@ -14,3 +26,5 @@ increaseButtonEl.addEventListener('click', function() {
     // set counter elemnet with new value
     counterValueEl.textContent = newValue;
 });
+
+
